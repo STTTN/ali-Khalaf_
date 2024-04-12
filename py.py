e2f1e24c96c144ad5ecb861c876c307c6fff261c
STTTN <98221923+STTTN@users.noreply.github.com>
2024-04-12 05:36:54 +0300

    py.py

--- py.py
+++ py.py
@@ -1,13 +1,7 @@
-from flask import Flask
-import threading
+from flask import Flask, request
 app = Flask(__name__)
 
-value = 0  # القيمة الافتراضية تكون صفر
-
-def reset_value():
-    global value
-    value = 0
-    print("تم إعادة تعيين القيمة إلى صفر.")
+value = 1  # القيمة الافتراضية تكون صفر
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_value():
@@ -19,9 +13,6 @@ def handle_value():
         if new_value is not None:
             try:
                 value = int(new_value)  # تحديث القيمة بالقيمة التي أدخلها المستخدم
-                print("تم تحديث القيمة بنجاح إلى:", value)
-                # إذا قمنا بتحديث القيمة، فنقوم بإعادة تعيينها إلى صفر بعد مرور خمس ثوانٍ
-                threading.Timer(5.0, reset_value).start()
                 return 'تم تحديث القيمة بنجاح إلى: {}'.format(value)
             except ValueError:
                 return 'خطأ: يجب إدخال قيمة صحيحة.'
@@ -29,4 +20,4 @@ def handle_value():
             return 'خطأ: يرجى إرسال قيمة جديدة.'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
